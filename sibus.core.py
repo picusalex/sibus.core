3d0817f2b51d0a61528b157d6b0ae9c7215ab673
picusalex <picusalex@yahoo.fr>
2017-09-01 19:13:07 +0200

    first commit

---
+++ sibus.core.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import signal
+import sys
+import time
+
+from sibus_lib import mylogger
+from sibus_lib import BusCore
+
+PROGRAM_NAME = "bus.core"
+logger = mylogger(PROGRAM_NAME)
+
+logger.info("####################################################")
+buscore = BusCore()
+buscore.start()
+
+def sigterm_handler(_signo=None, _stack_frame=None):
+    buscore.stop()
+    logger.info("Program terminated correctly")
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, sigterm_handler)
+
+try:
+    while 1:
+        time.sleep(10)
+except KeyboardInterrupt:
+    logger.info("Ctrl+C detected !")
+except Exception as e:
+    buscore.stop()
+    logger.exception("Program terminated incorrectly ! ", e)
+    sys.exit(1)
+finally:
+    sigterm_handler()
+
